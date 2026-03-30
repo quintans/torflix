4953dc6e1adbc6ea9013d8e865f14aa7d2df6ccd
Paulo Quintans Pereira <paulo.quintans@gmail.com>
2026-03-30 15:03:53 +0100

    refactor retry logic

--- internal/lib/retry/retry.go
+++ internal/lib/retry/retry.go
@@ -21,17 +21,54 @@ func NewPermanentError(err error) error {
 	return PermanentError{err: err}
 }
 
-type Option func(*Options)
+type Option func(*Retry)
 
-type Options struct {
+type Retry struct {
 	retries   int
 	delayFunc func(attempt int, err error) time.Duration
+}
+
+func New(options ...Option) *Retry {
+	opts := &Retry{
+		retries: 3,
+		delayFunc: func(int, error) time.Duration {
+			return 100 * time.Millisecond
+		},
+	}
+	for _, o := range options {
+		o(opts)
+	}
+	return opts
+}
+
+func (r *Retry) Do(f func() error) error {
+	var err error
+	for i := 0; r.retries == 0 || i <= r.retries; i++ {
+		err = f()
+		if err == nil {
+			return nil
+		}
+
+		var perr PermanentError
+		if errors.As(err, &perr) {
+			return perr.Unwrap()
+		}
+
+		if r.retries == 0 || i < r.retries {
+			delay := r.delayFunc(i+1, err)
+			if delay == 0 {
+				return err
+			}
+			time.Sleep(delay)
+		}
+	}
+	return err
 }
 
 // WithRetries sets the number of retries. If retries is 0, it will retry forever.
 // If not set, it will use a default of 3 retries.
 func WithRetries(retries int) Option {
-	return func(o *Options) {
+	return func(o *Retry) {
 		o.retries = retries
 	}
 }
@@ -39,7 +76,7 @@ func WithRetries(retries int) Option {
 // WithDelay sets the delay between retries.
 // If not set, it will use a default delay of 100 milliseconds.
 func WithDelay(delay time.Duration) Option {
-	return func(o *Options) {
+	return func(o *Retry) {
 		o.delayFunc = func(attempt int, err error) time.Duration {
 			return delay
 		}
@@ -49,7 +86,7 @@ func WithDelay(delay time.Duration) Option {
 // WithDelays sets the delays between retries.
 // The number of retries will be the number of delays.
 func WithDelays(delays ...time.Duration) Option {
-	return func(o *Options) {
+	return func(o *Retry) {
 		o.retries = len(delays)
 		o.delayFunc = func(attempt int, err error) time.Duration {
 			return delays[attempt-1]
@@ -61,7 +98,7 @@ func WithDelays(delays ...time.Duration) Option {
 // The number of retries will be infinite.
 // If the number of the retry is greater than the number of delays, it will use the last delay.
 func WithInfiniteDelays(delays ...time.Duration) Option {
-	return func(o *Options) {
+	return func(o *Retry) {
 		o.retries = 0
 		o.delayFunc = func(attempt int, err error) time.Duration {
 			if attempt > len(delays) {
@@ -75,41 +112,12 @@ func WithInfiniteDelays(delays ...time.Duration) Option {
 // WithDelayFunc sets the delay function between retries.
 // If the function returns 0, it will stop retrying.
 func WithDelayFunc(f func(retry int, err error) time.Duration) Option {
-	return func(o *Options) {
+	return func(o *Retry) {
 		o.delayFunc = f
 	}
 }
 
 func Do(f func() error, options ...Option) error {
-	opts := &Options{
-		retries: 3,
-		delayFunc: func(int, error) time.Duration {
-			return 100 * time.Millisecond
-		},
-	}
-	for _, o := range options {
-		o(opts)
-	}
-
-	var err error
-	for i := 0; opts.retries == 0 || i <= opts.retries; i++ {
-		err = f()
-		if err == nil {
-			return nil
-		}
-
-		var perr PermanentError
-		if errors.As(err, &perr) {
-			return perr.Unwrap()
-		}
-
-		if opts.retries == 0 || i < opts.retries {
-			delay := opts.delayFunc(i+1, err)
-			if delay == 0 {
-				return err
-			}
-			time.Sleep(delay)
-		}
-	}
-	return err
+	r := New(options...)
+	return r.Do(f)
 }
